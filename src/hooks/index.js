9e98a6e3ab26b57c1e67e223be7d1ff8b24b1239
Neil M <neilm813@gmail.com>
2022-04-21 16:24:33 -0700

    add custom hook example

---
+++ src/hooks/index.js
@@ -0,0 +1,7 @@
+/* 
+Shared custom hooks that multiple components could use.
+Custom hooks that are only for a specific component can be placed in that
+component's folder instead.
+*/
+
+export { useLocalStorage } from './useLocalStorage';
