6a62904689b3a6cfbedd2be034eda9604e742d5e
Neil M <neilm813@gmail.com>
2022-04-21 12:42:37 -0700

    reorder nav links

--- src/components/TopNav/index.js
+++ src/components/TopNav/index.js
@@ -13,9 +13,9 @@ export const TopNav = ({ position = 'fixed' }) => {
       </div>
       <nav className="row gap-md">
         <Link to="/">Home</Link>
-        <Link to="/launches/latest">Latest Launch</Link>
         <Link to="/launches/5eb87d13ffd86e000604b360">Famous Launch</Link>
         <Link to="/launches">Photographed Launches</Link>
+        <Link to="/launches/latest">Latest Launch</Link>
       </nav>
     </header>
   );
