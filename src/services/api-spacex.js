d13b4cfa0a71a8dd0343787594eef9dee657fa43
Neil M <neilm813@gmail.com>
2022-04-22 14:27:46 -0700

    rocket route

--- src/services/api-spacex.js
+++ src/services/api-spacex.js
@@ -47,6 +47,17 @@ export const getAllLaunches = async () => {
 };
 
 /**
+ * Gets a single rocket.
+ * @param {string} id The rocket id.
+ * @returns {Promise<Rocket>}
+ * @see [API Docs One Rocket](https://github.com/r-spacex/SpaceX-API/blob/master/docs/rockets/v4/one.md)
+ */
+export const getRocket = async (id) => {
+  const res = await http.get(`/rockets/${id}`);
+  return res.data;
+};
+
+/**
  * @typedef {object} Launch
  * @property {object} fairings
  * @property {boolean} fairings.reused
@@ -105,5 +116,86 @@ export const getAllLaunches = async () => {
  * @property {boolean} cores.landing_success
  * @property {string} cores.landing_type
  * @property {string} cores.landpad
+ * @property {string} id
+ */
+
+/**
+ * @typedef {object} Rocket
+ * @property {object} height
+ * @property {number} height.meters
+ * @property {number} height.feet
+ * @property {object} diameter
+ * @property {number} diameter.meters
+ * @property {number} diameter.feet
+ * @property {object} mass
+ * @property {number} mass.kg
+ * @property {number} mass.lb
+ * @property {object} first_stage
+ * @property {object} first_stage.thrust_sea_level
+ * @property {number} first_stage.thrust_sea_level.kN
+ * @property {number} first_stage.thrust_sea_level.lbf
+ * @property {object} first_stage.thrust_vacuum
+ * @property {number} first_stage.thrust_vacuum.kN
+ * @property {number} first_stage.thrust_vacuum.lbf
+ * @property {boolean} first_stage.reusable
+ * @property {number} first_stage.engines
+ * @property {number} first_stage.fuel_amount_tons
+ * @property {number} first_stage.burn_time_sec
+ * @property {object} second_stage
+ * @property {object} second_stage.thrust
+ * @property {number} second_stage.thrust.kN
+ * @property {number} second_stage.thrust.lbf
+ * @property {object} second_stage.payloads
+ * @property {object} second_stage.payloads.composite_fairing
+ * @property {object} second_stage.payloads.composite_fairing.height
+ * @property {number} second_stage.payloads.composite_fairing.height.meters
+ * @property {number} second_stage.payloads.composite_fairing.height.feet
+ * @property {object} second_stage.payloads.composite_fairing.diameter
+ * @property {number} second_stage.payloads.composite_fairing.diameter.meters
+ * @property {number} second_stage.payloads.composite_fairing.diameter.feet
+ * @property {string} second_stage.payloads.option_1
+ * @property {boolean} second_stage.reusable
+ * @property {number} second_stage.engines
+ * @property {number} second_stage.fuel_amount_tons
+ * @property {number} second_stage.burn_time_sec
+ * @property {object} engines
+ * @property {object} engines.isp
+ * @property {number} engines.isp.sea_level
+ * @property {number} engines.isp.vacuum
+ * @property {object} engines.thrust_sea_level
+ * @property {number} engines.thrust_sea_level.kN
+ * @property {number} engines.thrust_sea_level.lbf
+ * @property {object} engines.thrust_vacuum
+ * @property {number} engines.thrust_vacuum.kN
+ * @property {number} engines.thrust_vacuum.lbf
+ * @property {number} engines.number
+ * @property {string} engines.type
+ * @property {string} engines.version
+ * @property {string} engines.layout
+ * @property {number} engines.engine_loss_max
+ * @property {string} engines.propellant_1
+ * @property {string} engines.propellant_2
+ * @property {number} engines.thrust_to_weight
+ * @property {object} landing_legs
+ * @property {number} landing_legs.number
+ * @property {null} landing_legs.material
+ * @property {object[]} payload_weights
+ * @property {string} payload_weights.id
+ * @property {string} payload_weights.name
+ * @property {number} payload_weights.kg
+ * @property {number} payload_weights.lb
+ * @property {string[]} flickr_images
+ * @property {string} name
+ * @property {string} type
+ * @property {boolean} active
+ * @property {number} stages
+ * @property {number} boosters
+ * @property {number} cost_per_launch
+ * @property {number} success_rate_pct
+ * @property {string} first_flight
+ * @property {string} country
+ * @property {string} company
+ * @property {string} wikipedia
+ * @property {string} description
  * @property {string} id
  */
